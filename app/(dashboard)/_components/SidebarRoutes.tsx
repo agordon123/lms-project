178873576e341b4bad09c3ce4da11a0002d129f2
adam <adamg42685@gmail.com>
2023-09-19 05:06:56 -0400

    Dynamic Layout Setup w/ Multi-routes

--- app/(dashboard)/_components/SidebarRoutes.tsx
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -1,20 +1,39 @@
-import { Compass, Layout } from "lucide-react"
+"use client";
 
+import { BarChart, Compass, Layout, List } from "lucide-react";
+import SidebarItem from "./SidebarItem";
+import { usePathname } from "next/navigation";
 const guestRoutes = [
-    {
-        icon:Layout,
-        label:"Dashboard",
-        href:"/"
-    },
-    {
-        icon:Compass,
-        label:"Browse",
-        href:"/search"
-    }
-]
-
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+const teacherRoutes = [
+  { icon: List, label: "Courses", href: "/teacher/courses" },
+  { icon: BarChart, label: "Analytics", href: "/teacher/analytics" },
+];
 export const SidebarRoutes = () => {
-    return(
-        <div className="flex flex-col w-full"> 
-        Routes!</div>
-    )}
+
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
