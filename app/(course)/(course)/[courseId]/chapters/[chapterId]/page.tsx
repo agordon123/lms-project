af9142a924d733925f48d42f3536e957c02a9b57
adam <adamg42685@gmail.com>
2023-09-23 16:40:55 -0400

    Video Player UI

---
+++ app/(course)/(course)/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,80 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+import { getChapter } from "@/actions/getChapter";
+import { Banner } from "@/components/Banner";
+import { CourseEnrollButton } from "./_components/CourseEnrollButton";
+import { CourseProgressButton } from "./_components/CourseProgressButton";
+import { VideoPlayer } from "./_components/VideoPlayer";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You Already Comp[eted this Chapter" />
+      )}
+      {isLocked && <Banner variant="warning" label="This Chapter is Locked" />}
+      <div className="flex flex-col max-w-4x mx-auto pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+            playbackId={muxData?.playbackId}
+          />
+        </div>
+      </div>
+      <div>
+        <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+            <h2 className="text-2xl font-semibold mb-2">
+                {chapter.title}
+            </h2>
+            {purchase ? (
+                <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isCompleted={!!userProgress?.isCompleted}
+                />
+            ):(
+                <CourseEnrollButton courseId={params.courseId} price={course.price!} />
+            )}
+        </div>
+        <Seperator />
+      </div>
+    </div>
+  );
+};
+export default ChapterIdPage;
