dbb7b9d002f09162a8d446ff9492f0d4158c1bc3
adam <adamg42685@gmail.com>
2023-09-23 20:03:26 -0400

    changed schema, had missing layout file

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,24 @@
-import { isTeacher } from "@/lib/teacher";
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import Navbar from "./_components/Navbar";
+import { Sidebar } from "./_components/Sidebar";
 
-const TeacherLayout = ({
+const DashboardLayout = ({
   children
 }: {
   children: React.ReactNode;
 }) => {
-  const { userId } = auth();
-
-  if (!isTeacher(userId)) {
-    return redirect("/");
-  }
-
-  return <>{children}</>
+  return ( 
+    <div className="h-full">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      <main className="md:pl-56 pt-[80px] h-full">
+        {children}
+      </main>
+    </div>
+   );
 }
-
-export default TeacherLayout;
+ 
+export default DashboardLayout;
